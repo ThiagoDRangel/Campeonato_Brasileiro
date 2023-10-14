db0a05c0a087674282ea493ce732dbd086dbb0e0
ThiagoDRangel <info.tec.campos@gmail.com>
2023-10-14 14:07:15 -0300

    fix: routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,20 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import Leaderboard from "./pages/Leaderboard";
+import MatchSettings from "./pages/MatchSettings";
+import Games from "./pages/Games";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="matches/settings" element={<MatchSettings />} />
         <Route path="leaderboard" element={<Leaderboard />} />
+        <Route path="/matches" element={<Games />} />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
